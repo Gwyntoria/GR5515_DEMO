70de4dcccec3407492078ab5dc5ae94707321aa2
huxinze <huxinze@gmail.com>
2023-10-10 16:32:08 +0800

    Add some comments about rtc module

--- projects/ble/ble_peripheral/photon_smart_ring/Src/user/user_rtc.h
+++ projects/ble/ble_peripheral/photon_smart_ring/Src/user/user_rtc.h
@@ -21,12 +21,29 @@ uint16_t rtc_deinit();
 
 RtcStatus rtc_get_rtc_status();
 
-uint16_t rtc_set_tick_alarm(uint32_t tick_peirod);
+/**
+ * @brief Set the clock alarm at millisecond intervals
+ *
+ * @param tick_interval_ms The time interval between two alarms
+ * @return uint16_t Result of operation
+ */
+uint16_t rtc_set_tick_alarm(uint32_t tick_interval_ms);
 uint16_t rtc_disable_tick_alarm();
 
+/**
+ * @brief Set a clock alarm triggered by a date
+ *
+ * @param alarm Date that triggered the clock alarm
+ * @return uint16_t Result of operation
+ */
 uint16_t rtc_set_date_alarm(app_rtc_alarm_t* alarm);
 uint16_t rtc_disable_date_alarm();
 
+/**
+ * @brief Disable all clock alarms
+ *
+ * @return uint16_t Result of operation
+ */
 uint16_t rtc_disable_all_alarm();
 
 uint16_t rtc_get_current_time(app_rtc_time_t* time);
