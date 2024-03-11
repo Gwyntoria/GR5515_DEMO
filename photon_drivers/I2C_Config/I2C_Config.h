455db293d4d28653fa99c13602970d70ab50985c
Gwyntoria <45159649+Gwyntoria@users.noreply.github.com>
2024-03-11 11:35:01 +0800

    修改六轴和温度传感器的I2C定义

--- photon_drivers/I2C_Config/I2C_Config.h
+++ photon_drivers/I2C_Config/I2C_Config.h
@@ -3,27 +3,24 @@
 
 #include "stdint.h"
 
-
+#define LSM_I2C_ID				APP_I2C_ID_0 // 六轴
+#define Nst_I2C_ID				APP_I2C_ID_1 // 温度
 
 /** i2c scl/sda config define*/
 #define I2C0_SCL_PIN			APP_IO_PIN_10
 #define I2C0_SDA_PIN			APP_IO_PIN_11
 #define I2C0_IO_MUX				APP_IO_MUX_4
-#define I2C0_ID                 APP_I2C_ID_0
+#define I2C0_ID                 LSM_I2C_ID
 
 #define I2C1_SCL_PIN			APP_IO_PIN_30
 #define I2C1_SDA_PIN			APP_IO_PIN_26
 #define I2C1_IO_MUX				APP_IO_MUX_0
-#define I2C1_ID                 APP_I2C_ID_1
+#define I2C1_ID                 Nst_I2C_ID
 
 
 #define I2C0_OWN_ADDR			0xA1
 #define I2C1_OWN_ADDR			0xA2
 
-
-#define Nst_I2C_ID				APP_I2C_ID_1
-#define LSM_I2C_ID				APP_I2C_ID_0
-//#define ST25DV_I2C_ID			APP_I2C_ID_0
 void GT_I2C_Init(void);
 
 #endif
