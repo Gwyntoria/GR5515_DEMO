fca0c4a83bc93abf19501a4462f6d93936c6c1af
Gwyntoria <45159649+Gwyntoria@users.noreply.github.com>
2024-03-13 11:36:02 +0800

    修改外部flash中关于分区信息，减少log占用空间

--- projects/ble/ble_peripheral/photon_smart_ring/Src/user/user_file_sys.h
+++ projects/ble/ble_peripheral/photon_smart_ring/Src/user/user_file_sys.h
@@ -11,12 +11,12 @@
 // config [0, 1) 4K
 #define FS_SECTOR_OP_CFG 0
 #define FS_SECTOR_ED_CFG 1
-// log [2, 1024) 4M
+// log [2, 255) 1M
 #define FS_SECTOR_OP_LOG 2
-#define FS_SECTOR_ED_LOG 1024
-// data [1025, 2560) 6M
-#define FS_SECTOR_OP_DAT 1025
-#define FS_SECTOR_ED_DAT 2560
+#define FS_SECTOR_ED_LOG 255
+// data [256, 1280) 4M
+#define FS_SECTOR_OP_DAT 256
+#define FS_SECTOR_ED_DAT 1280
 
 #define FS_BLOCK_SIZE  GD25LE128E_BLOCK_64K_SIZE // 64 KB
 #define FS_SECTOR_SIZE GD25LE128E_SECTOR_SIZE    // 4 KB
@@ -64,29 +64,29 @@ typedef enum FlashZone {
 } FlashZone;
 
 typedef struct FlashZoneConfig {
-    uint8_t  status; // @ref PageStatus
-    uint16_t pack_len;
-    uint32_t wt_addr; // config 所在扇区可继续写入的地址（reserved）
-    uint8_t  ver_major;
+    uint8_t  status;    // @ref PageStatus
+    uint16_t pack_len;  // config信息长度
+    uint32_t wt_addr;   // config 所在扇区可继续写入的地址（reserved）
+    uint8_t  ver_major; // 固件版本号
     uint8_t  ver_minor;
     uint8_t  ver_build;
     uint8_t  ver_exten;
-    uint8_t  activation;
+    uint8_t  activation; // 激活状态
 
 } FlashZoneConfig;
 
 typedef struct FlashZoneInfo {
-    uint8_t  status; // @ref PageStatus
-    uint8_t  type;   // @ref FlashZone
-    uint16_t pack_len;
-    uint16_t sector_op;
-    uint16_t sector_ed;
-    uint16_t rd_sector;
-    uint16_t wt_sector;
-    uint16_t wt_page; // @ref GD25LE128E_PAGE_CNT_PER_SECTOR
-    uint32_t rd_addr; //
-    uint32_t wt_addr;
-    uint8_t  cycle_state;
+    uint8_t  status;      // @ref PageStatus
+    uint8_t  type;        // @ref FlashZone
+    uint16_t pack_len;    // 分区信息数据长度
+    uint16_t sector_op;   // 分区起始扇区
+    uint16_t sector_ed;   // 分区结束扇区
+    uint16_t rd_sector;   // 读取扇区标志
+    uint16_t wt_sector;   // 写入扇区标志
+    uint16_t wt_page;     // @ref GD25LE128E_PAGE_CNT_PER_SECTOR
+    uint32_t rd_addr;     // 起始可读地址
+    uint32_t wt_addr;     // 起始写入地址
+    uint8_t  cycle_state; // 分区循环写入状态
 
 } FlashZoneInfo;
 
