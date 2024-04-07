6c3dd3a3d5e51333431130753cfa5be8c7d84555
Gwyntoria <45159649+Gwyntoria@users.noreply.github.com>
2024-04-07 14:26:43 +0800

    修复电量读取时，因DMA通道冲突造成的异步读写失败

--- photon_drivers/STNS01/stns01.c
+++ photon_drivers/STNS01/stns01.c
@@ -13,64 +13,52 @@
 #include "app_adc.h"
 #include "app_log.h"
 
-uint8_t  covn_done = 0;
-uint16_t conversion[TEST_CONV_LENGTH];
-uint16_t adc_channel0_buf[TEST_CONV_LENGTH];
-uint16_t adc_channel1_buf[TEST_CONV_LENGTH];
-double   voltage[TEST_CONV_LENGTH];
+static uint8_t s_covn_done = 0;
+
+uint16_t adc_channel0_buf[ADC_CONV_LENGTH];
+uint16_t adc_channel1_buf[ADC_CONV_LENGTH];
+double   voltage[ADC_CONV_LENGTH];
 
 app_adc_samle_node_t sample_node0;
 app_adc_samle_node_t sample_node1;
 
 void stns01_adc_evt_handler(app_adc_evt_t* p_evt) {
     if (p_evt->type == APP_ADC_EVT_CONV_CPLT) {
-        covn_done = 1;
+        s_covn_done = 1;
     }
 }
 
 void stns01_init(void) {
-    app_adc_params_t stns01_vbat_struct /* = DEFAULT_PARAM_CONFIG*/;
-    app_adc_params_t stns01_vin_struct;
+    memset(adc_channel0_buf, 0, sizeof(ADC_CONV_LENGTH));
+    memset(adc_channel1_buf, 0, sizeof(ADC_CONV_LENGTH));
+    memset(voltage, 0, sizeof(ADC_CONV_LENGTH));
 
+    app_adc_params_t stns01_vbat_struct;
     stns01_vbat_struct.pin_cfg.channel_n.type = APP_IO_TYPE_MSIO;
     stns01_vbat_struct.pin_cfg.channel_n.pin  = MSIO_PIN_0;
     stns01_vbat_struct.pin_cfg.channel_n.mux  = APP_IO_MUX_7;
-    //	stns01_vbat_struct.pin_cfg.channel_p.type 	= APP_IO_TYPE_MSIO;
-    //	stns01_vbat_struct.pin_cfg.channel_p.pin  	= MSIO_PIN_0;
-    //	stns01_vbat_struct.pin_cfg.channel_p.mux  	= APP_IO_MUX_7;
-    stns01_vbat_struct.use_mode.dma_channel   = DMA_Channel0;
+    stns01_vbat_struct.pin_cfg.channel_p.type = APP_IO_TYPE_MSIO;
+    stns01_vbat_struct.pin_cfg.channel_p.pin  = MSIO_PIN_0;
+    stns01_vbat_struct.pin_cfg.channel_p.mux  = APP_IO_MUX_7;
     stns01_vbat_struct.use_mode.type          = APP_ADC_TYPE_DMA;
+    stns01_vbat_struct.use_mode.dma_channel   = DMA_Channel3;
     stns01_vbat_struct.init.channel_n         = ADC_INPUT_SRC_IO0;
-    //	stns01_vbat_struct.init.channel_p 			= ADC_INPUT_SRC_IO0;
+    stns01_vbat_struct.init.channel_p         = ADC_INPUT_SRC_IO0;
     stns01_vbat_struct.init.input_mode        = ADC_INPUT_SINGLE;
     stns01_vbat_struct.init.ref_source        = ADC_REF_SRC_BUF_INT;
     stns01_vbat_struct.init.ref_value         = LL_ADC_REF_VALUE_1P6;
     stns01_vbat_struct.init.clock             = LL_ADC_CLK_1P6;
     app_adc_init(&stns01_vbat_struct, stns01_adc_evt_handler);
 
-    stns01_vin_struct.pin_cfg.channel_n.type = APP_IO_TYPE_MSIO;
-    stns01_vin_struct.pin_cfg.channel_n.pin  = MSIO_PIN_1;
-    stns01_vin_struct.pin_cfg.channel_n.mux  = APP_IO_MUX_7;
-    //	stns01_vin_struct.pin_cfg.channel_p.type 	= APP_IO_TYPE_MSIO;
-    //	stns01_vin_struct.pin_cfg.channel_p.pin  	= MSIO_PIN_1;
-    //	stns01_vin_struct.pin_cfg.channel_p.mux  	= APP_IO_MUX_7;
-    stns01_vin_struct.use_mode.dma_channel   = DMA_Channel0;
-    stns01_vin_struct.use_mode.type          = APP_ADC_TYPE_DMA;
-    stns01_vin_struct.init.channel_n         = ADC_INPUT_SRC_IO1;
-    //	stns01_vin_struct.init.channel_p 			= ADC_INPUT_SRC_IO1;
-    stns01_vin_struct.init.input_mode        = ADC_INPUT_SINGLE;
-    stns01_vin_struct.init.ref_source        = ADC_REF_SRC_BUF_INT;
-    stns01_vin_struct.init.ref_value         = LL_ADC_REF_VALUE_1P6;
-    stns01_vin_struct.init.clock             = LL_ADC_CLK_1P6;
-    app_adc_init(&stns01_vin_struct, stns01_adc_evt_handler);
-
+    // battery
     sample_node0.channel = ADC_INPUT_SRC_IO0;
-    sample_node0.p_buf   = &adc_channel0_buf[0];
-    sample_node0.len     = TEST_CONV_LENGTH;
+    sample_node0.p_buf   = adc_channel0_buf;
+    sample_node0.len     = ADC_CONV_LENGTH;
 
+    // charging
     sample_node1.channel = ADC_INPUT_SRC_IO1;
-    sample_node1.p_buf   = &adc_channel1_buf[0];
-    sample_node1.len     = TEST_CONV_LENGTH;
+    sample_node1.p_buf   = adc_channel1_buf;
+    sample_node1.len     = ADC_CONV_LENGTH;
 
     sample_node0.next = &sample_node1;
 
@@ -78,55 +66,46 @@ void stns01_init(void) {
 }
 
 double stns01_get_voltage(uint8_t channel) {
-    double get_voltage  = 0.0;
-    double send_voltage = 0.0;
+    double voltage_avg  = 0.0;
 
-    memset(adc_channel0_buf, 0, sizeof(adc_channel0_buf));
-    memset(adc_channel1_buf, 0, sizeof(adc_channel1_buf));
-    covn_done = 0;
+    memset(adc_channel0_buf, 0, sizeof(ADC_CONV_LENGTH));
+    memset(adc_channel1_buf, 0, sizeof(ADC_CONV_LENGTH));
+    memset(voltage, 0, sizeof(ADC_CONV_LENGTH));
+    
+    s_covn_done = 0;
 
     app_adc_multi_channel_conversion_async(&sample_node0, 2);
-    while (covn_done == 0) {
-        delay_us(1);
+    while (s_covn_done == 0) {
+        APP_LOG_DEBUG("Waiting for conversion done...");
+        delay_ms(5);
     }
 
     if (channel == STNS01_CHANNEL_BATTERY) {
-        app_adc_voltage_intern(adc_channel0_buf, voltage, TEST_CONV_LENGTH);
-        for (uint32_t i = 0; i < TEST_CONV_LENGTH; i++) // ignore the first 3 abnormal values
-        {
-            if (i > 5 && i < 11) {
-                get_voltage = get_voltage + voltage[i];
-            }
-            delay_us(1000);
-        }
-        send_voltage = get_voltage / 5 * 2;
-
-        return send_voltage;
+        app_adc_voltage_intern(adc_channel0_buf, voltage, ADC_CONV_LENGTH);
 
     } else if (channel == STNS01_CHANNEL_CHARGING) {
-        app_adc_voltage_intern(adc_channel1_buf, voltage, TEST_CONV_LENGTH);
-        for (uint32_t i = 0; i < TEST_CONV_LENGTH; i++) // ignore the first 3 abnormal values
-        {
-            if (i > 5 && i < 11) {
-                get_voltage = get_voltage + voltage[i];
-            }
-            delay_us(1000);
-        }
-        send_voltage = get_voltage / 5;
+        app_adc_voltage_intern(adc_channel1_buf, voltage, ADC_CONV_LENGTH);
 
-        return send_voltage;
     } else {
         APP_LOG_ERROR("Unknown channel[%d]", channel);
-        return -1;
+        return -1.0;
     }
+
+    for (uint32_t i = ADC_CONV_IGNORE; i < ADC_CONV_LENGTH; i++) {
+        voltage_avg += voltage[i];
+    }
+
+    voltage_avg = voltage_avg / (ADC_CONV_LENGTH - ADC_CONV_IGNORE);
+
+    return voltage_avg * 2.0;
 }
 
 void stns01_test(void) {
     double Power1 = 0.0, Power2 = 0.0;
 
     Power1 = stns01_get_voltage(STNS01_CHANNEL_BATTERY); // 获取电池电压
-    APP_LOG_DEBUG("Now VABAT Power is %.2fV", Power1);
+    APP_LOG_DEBUG("Now VABAT Power is %.3fV", Power1);
 
     Power2 = stns01_get_voltage(STNS01_CHANNEL_CHARGING); // 获取充电时的电压
-    APP_LOG_DEBUG("Now BATMS Power is %.2fV", Power2);
+    APP_LOG_DEBUG("Now BATMS Power is %.3fV", Power2);
 }
--- photon_drivers/STNS01/stns01.h
+++ photon_drivers/STNS01/stns01.h
@@ -4,25 +4,14 @@
 #include <stdint.h>
 #include <string.h>
 
-#define TEST_CONV_LENGTH (16UL)
+#define ADC_CONV_LENGTH (64UL)
+#define ADC_CONV_IGNORE (5UL)
 
 #define STNS01_CHANNEL_BATTERY  (0) // VABAT
 #define STNS01_CHANNEL_CHARGING (1) // BATMS
 
-#define DEFAULT_IO_CONFIG                              \
-    {                                                  \
-        {APP_IO_TYPE_MSIO, APP_IO_MUX_7, MSIO_PIN_0},  \
-        { APP_IO_TYPE_MSIO, APP_IO_MUX_7, MSIO_PIN_0 } \
-    }
-#define DEFAULT_MODE_CONFIG \
-    { APP_ADC_TYPE_DMA, DMA_Channel0 }
-#define DEFAULT_ADC_CONFIG \
-    { ADC_INPUT_SRC_IO0, ADC_INPUT_SRC_IO0, ADC_INPUT_SINGLE, ADC_REF_SRC_BUF_INT, ADC_REF_VALUE_1P6, ADC_CLK_1P6M }
-#define DEFAULT_PARAM_CONFIG \
-    { DEFAULT_IO_CONFIG, DEFAULT_MODE_CONFIG, DEFAULT_ADC_CONFIG }
-
 void   stns01_init(void);
-double stns01_get_voltage(uint8_t Get_channel);
+double stns01_get_voltage(uint8_t channel);
 
 void stns01_test(void);
 
