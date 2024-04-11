d9be9038ad873eea371686b699b30e6e60965bcf
Gwyntoria <45159649+Gwyntoria@users.noreply.github.com>
2024-04-11 14:14:09 +0800

    修改充电芯片的通道定义

--- photon_drivers/STNS01/stns01.h
+++ photon_drivers/STNS01/stns01.h
@@ -7,8 +7,8 @@
 #define ADC_CONV_LENGTH (64UL)
 #define ADC_CONV_IGNORE (5UL)
 
-#define STNS01_CHANNEL_BATTERY  (0) // VABAT
-#define STNS01_CHANNEL_CHARGING (1) // BATMS
+#define STNS01_CHANNEL_CHARGING (0) // BATMS
+#define STNS01_CHANNEL_BATTERY  (1) // VABAT
 
 void   stns01_init(void);
 double stns01_get_voltage(uint8_t channel);
