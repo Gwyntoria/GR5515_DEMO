7f318ea45ba0ce475eec2e27626a6c5bc7b3e0a1
Gwyntoria <45159649+Gwyntoria@users.noreply.github.com>
2024-05-14 16:38:12 +0800

    完成模块初始化和test

--- projects/ble/ble_peripheral/photon_smart_ring/Src/user/main.c
+++ projects/ble/ble_peripheral/photon_smart_ring/Src/user/main.c
@@ -56,16 +56,18 @@
 #include "gh3x2x_demo_config.h"
 
 #include "user_common.h"
+#include "user_file_sys.h"
 #include "user_func_ctrl.h"
 #include "user_rtc.h"
 #include "user_timer.h"
+#include "user_i2c.h"
+#include "user_lsm6dso.h"
+#include "user_nfc.h"
+#include "user_data_center.h"
 
-#include "PowerSample/ADC.h"
-#include "ST25DV04K/ST25DV.h"
-#include "I2C_Config/I2C_Config.h"
 #include "GD25LE128E/Flash_Spi.h"
-#include "LSM6DSO/LSM6DSOWTR.h"
 #include "NST112/nst112x.h"
+#include "STNS01/stns01.h"
 
 /*
  * GLOBAL VARIABLE DEFINITIONS
@@ -77,6 +79,8 @@ extern gattc_cb_fun_t       app_gattc_callback;
 extern sec_cb_fun_t         app_sec_callback;
 
 extern GU8 g_uchGh3x2xIntCallBackIsCalled;
+
+char APP_VERSION[32];
 
 /*
  * LOCAL VARIABLE DEFINITIONS
@@ -94,69 +98,45 @@ static app_callback_t s_app_ble_callback = {
     .app_sec_callback          = &app_sec_callback,
 };
 
-static char APP_VERSION[16];
-
 int main(void) {
-    // NFC tag config
-    uint8_t  text_arry[22] = {0xE1, 0x40, 0x40, 0x01, 0x03, 0x0F, 0xD1, 0x06,
-                              0x06, 0x57, 0x4C, 0x43, 0x43, 0x41, 0x50, WLCCAP_NDEF_PROTOCOL_VERSION,
-                              0x57, 0x07, 0x1E, 0x03, 0x0A, 0xFE};
-    double   Power1 = 0.0, Power2 = 0.0;
-    double   Temperature[4]           = {0.0, 0.0, 0.0, 0.0};
-    uint16_t LSM6D_Temp               = 0;
-    uint16_t LSM6D_Data[6]            = {0};
-    uint32_t Flash_Address            = 0x0;
-    uint8_t  Flash_Write_Data_Arry[8] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-    uint8_t  Flash_Read_Data_Arry[8]  = {0};
-    uint8_t  i                        = 0;
+    memset(APP_VERSION, 0, sizeof(APP_VERSION));
+    sprintf(APP_VERSION, "%u.%u.%u.%lu", VER_MAJOR, VER_MINOR, VER_BUILD, VER_EXTEN);
 
-    APP_LOG_INFO("App Version: %s\r\n", APP_VERSION);
-    sprintf(APP_VERSION, "%d.%d.%d", VER_MAJOR, VER_MINOR, VER_BUILD);
+    user_rtc_init();   // RTC init
+    app_periph_init(); // Initialize user peripherals.
 
-    // Initialize user peripherals.
-    app_periph_init();
+    APP_LOG_INFO("Firmware Version: %s", APP_VERSION);
 
-    // Initialize ble stack.
-    ble_stack_init(&s_app_ble_callback, &heaps_table); /*< init ble stack*/
+    ble_stack_init(&s_app_ble_callback, &heaps_table); // Initialize ble stack.
 
-    GT_I2C_Init();      // I2C init （必须最早初始化）
-    Nst112_four_Init(); // 测温芯片初始化
-    LSM6DSOWTR_Init();  // 六轴初始化
-    ADC_Config_Init();  // 电池测量初始化
-    Flash_Config();     // FLASH Init
-    ST25DV_ReadUid();   // NFC 读取 Uid
-    
-    Gh3x2xDemoInit();   // Gh3x2x init
-    user_rtc_init();    // RTC init
-    func_ctrl_init();   // 功能控制模块初始化
+    user_i2c_init(); // I2C init
 
-    // // flash sample
-    // Flash_Read_Data(Flash_Address, Flash_Read_Data_Arry, 8);
-    // printf("Flash init Read Data is:\r\n");
-    // for (i = 0; i < 8; i++) {
-    //     printf("Flash_Read_Data_Arry[%d] = %d\r\n", i, Flash_Read_Data_Arry[i]);
-    // }
+    //* i2c
+    lsm6dso_init(); // 六轴 LSM6DSO
+    nst112_init();  // 测温芯片初始化
+    user_nfc_init(); // NFC tag 初始化
 
-    // Flash_Write_Data(Flash_Address, Flash_Write_Data_Arry, 8);
-    // Flash_Read_Data(Flash_Address, Flash_Read_Data_Arry, 8);
-    // printf("Flash Write after Read Data is:\r\n");
-    // for (i = 0; i < 8; i++) {
-    //     printf("Flash_Read_Data_Arry[%d] = %d\r\n", i, Flash_Read_Data_Arry[i]);
-    // }
+    //* spi
+    // ufs_reinit();
+    ufs_init();       // 文件系统初始化
+    Gh3x2xDemoInit(); // Gh3x2x init
 
-    // Flash_Sector_Erase(0);
-    // Flash_Read_Data(Flash_Address, Flash_Read_Data_Arry, 8);
-    // printf("Flash Erase after Read Data is:\r\n");
-    // for (i = 0; i < 8; i++) {
-    //     printf("Flash_Read_Data_Arry[%d] = %d\r\n", i, Flash_Read_Data_Arry[i]);
-    // }
+    //* adc
+    stns01_init(); // 电池测量初始化
 
-    // //	ST25DV_ReadUid();
-    // ST25DV_WriteData(0, text_arry, sizeof(text_arry)); // NFC write data
-    // delay_ms(250);
+    func_ctrl_init(); // 功能控制模块初始化
+    udm_init();       // 数据管理模块初始化
 
-    // func_ctrl_start(kFuncOptAdt);
+    // ufs_test(); // 文件系统测试
+    // udm_test(); // 数据管理模块测试
+    // nst112x_test(); // 温度读取test
+    // lsm6dso_test(); // 6轴传感器读取test
+    // stns01_test();  // 电量读取test
+    // user_nfc_test(); // NFC测试
 
+    // delay_ms(500);
+
+    // flash_func_test();
 
     while (1) {
 
@@ -164,46 +144,16 @@ int main(void) {
             Gh3x2xDemoInterruptProcess();
         }
 
-        func_ctrl_handler();
+        // func_ctrl_handler(); // 功能控制模块处理
+        // func_ctrl_test(); // 功能控制模块测试
 
-        // Temperature[0] = Get_nst112x_temperature(Nst112A_I2C_Addr);
-        // Temperature[1] = Get_nst112x_temperature(Nst112B_I2C_Addr);
-        // Temperature[2] = Get_nst112x_temperature(Nst112C_I2C_Addr);
-        // Temperature[3] = Get_nst112x_temperature(Nst112D_I2C_Addr);
-        // printf("Temperature0 = %.2f  Temperature1 = %.2f  Temperature2 = %.2f  Temperature3 = %.2f\r\n",
-        //        Temperature[0],
-        //        Temperature[1],
-        //        Temperature[2],
-        //        Temperature[3]);
+        // lsm6dso_get_event_status();
 
-        // // 电池 ADC
-        // Power1 = Get_adc_Voltage(0); // 获取电池电压
-        // printf("Now VABAT Power is %.2f\r\n", Power1);
-        // Power2 = Get_adc_Voltage(1); // 获取充电时的电压
-        // printf("Now BATMS Power is %.2f\r\n", Power2);
-
-        // // 六轴
-        // LSM6D_Temp = LSM6DSOWTR_Read_Data(LSM6DSO_OUT_TEMP_L); // 获取温度
-        // printf("Now LSM6D_Temp is %d\r\n", LSM6D_Temp);
-        // LSM6D_Data[0] = LSM6DSOWTR_Read_Data(LSM6DSO_OUTX_L_G);
-        // printf("Now LSM6DSO_OUTX_L_G is %d\r\n", LSM6D_Data[0]);
-        // LSM6D_Data[1] = LSM6DSOWTR_Read_Data(LSM6DSO_OUTY_L_G);
-        // printf("Now LSM6DSO_OUTY_L_G is %d\r\n", LSM6D_Data[1]);
-        // LSM6D_Data[2] = LSM6DSOWTR_Read_Data(LSM6DSO_OUTZ_L_G);
-        // printf("Now LSM6DSO_OUTZ_L_G is %d\r\n", LSM6D_Data[2]);
-        // LSM6D_Data[3] = LSM6DSOWTR_Read_Data(LSM6DSO_OUTX_L_A);
-        // printf("Now LSM6DSO_OUTX_L_A is %d\r\n", LSM6D_Data[3]);
-        // LSM6D_Data[4] = LSM6DSOWTR_Read_Data(LSM6DSO_OUTY_L_A);
-        // printf("Now LSM6DSO_OUTY_L_A is %d\r\n", LSM6D_Data[4]);
-        // LSM6D_Data[5] = LSM6DSOWTR_Read_Data(LSM6DSO_OUTZ_L_A);
-        // printf("Now LSM6DSO_OUTZ_L_A is %d\r\n", LSM6D_Data[5]);
-
-
-
-        app_log_flush();     // 刷新log缓存
+        app_log_flush(); // 刷新log缓存
+        app_log_store_schedule();
         pwr_mgmt_schedule(); // 电源管理调度，负责管理查询是否可以进入睡眠
         dfu_schedule();
 
-        delay_ms(100);
+        delay_ms(150);
     }
 }
